f4ca78e09c544fb6c50ac512d40bf2acd75edabe
Zeliq <116642127+Zeliq@users.noreply.github.com>
2024-04-23 01:51:06 +0530

    Update FishFresh.tsx

--- src/components/projects/FishFresh.tsx
+++ src/components/projects/FishFresh.tsx
@@ -37,11 +37,13 @@ const FishFresh: React.FC = () => {
           </h3>
           <p className="text-white/50 font-medium leading-7 flex mt-5 text-lg md:text-xl">
             The fishing industry is one of the most rapidly expanding industries. Fish consumption has increased all over the world. It is critical to maintain quality until the product reaches the consumer.From the catch to the consumer, the quality of a fish can be influenced primarily by handling, processing, and storage processes. The retention duration and storage temperature of post-harvested fish are important elements in maintaining the product's final quality. This study provides an image processing and deep learning method for image segmentation and fish freshness prediction that is totally automatic, efficient, and non-destructive. As soon as the image of the fish is sent to the network, it automatically classifies it as fresh or not fresh. The accuracy of the CNN-based approach to determining fish freshness was around 87.5%. By using a deep learning model, the accuracy increased by up to 100% on the dataset collected. </p>
-          <h3 className="text-white/50 font-medium leading-7 flex mt-5 text-lg md:text-xl">Revolutionizing Fish Freshness Assessment</h3>
+          <h3 className="mt-5 font-semibold text-2xl text-white">Revolutionizing Fish Freshness Assessment</h3>
             <br />
             <br />
+           <p className="text-white/50 font-medium leading-7 flex mt-5 text-lg md:text-xl">
             Our project pioneers a cutting-edge method leveraging image processing and deep learning techniques to automatically categorize fish as fresh or not. With an astounding accuracy of up to 100% on our meticulously curated dataset, we're reshaping the landscape of fish quality assessment.
-            <br />
+            </p>
+             <br />
     
       
           <br />
